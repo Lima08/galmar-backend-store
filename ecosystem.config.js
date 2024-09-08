26aa4a853565e23152d2573d4b5bb8e7f1e83167
Lima08 <joaopaulo.gomeslima8@gmail.com>
2024-09-08 20:31:28 -0300

    feat: add pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,34 @@
 module.exports = {
   apps: [
     {
-      name: 'medusa-admin', // Nome da aplicação
+      name: 'medusa-backend', // Nome da aplicação
       script: 'npm', // Script de inicialização
       args: 'start', // Argumento para o npm start
       instances: 1, // Número de instâncias (use "max" para usar todos os núcleos da CPU)
       autorestart: true, // Reinicia automaticamente em caso de falha
       watch: false, // Não observa mudanças nos arquivos
-      // max_memory_restart: '500M',      // Reinicia se ultrapassar 500MB de uso de memória
+      max_memory_restart: '500M',      // Reinicia se ultrapassar 500MB de uso de memória
       env: {
-        NODE_ENV: 'development', // Ambiente de desenvolvimento
-        MEDUSA_API_URL: 'http://localhost:9000', // URL da API Medusa
-        ADMIN_PORT: 7000 // Porta onde o admin será executado
+        NODE_ENV: 'development',           // Ambiente de desenvolvimento
+        DATABASE_TYPE: 'postgres',         // Configuração de banco de dados
+        DATABASE_URL: 'postgresql://postgres.vspkvsxxytxppdgujdni:@Galmar2552@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',
+        MEDUSA_ADMIN_ONBOARDING_TYPE: 'nextjs',
+        STORE_CORS: '/http://.+/',
+        ADMIN_CORS: '/http://.+/',
+        MEDUSA_ADMIN_ONBOARDING_NEXTJS_DIRECTORY: 'galmar-frontend-store',
+        MEDUSA_BACKEND_URL: 'http://44.203.95.169:9000',
+        JWT_SECRET: 'galmar',
+        COOKIE_SECRET: 'galmar'
       },
       env_production: {
-        NODE_ENV: 'production', // Ambiente de produção
-        DATABASE_TYPE: 'postgres', // Tipo de banco de dados
-        DATABASE_URL:
-          'postgresql//postgres.vspkvsxxytxppdgujdni:@Galmar2552@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',
+        NODE_ENV: 'production',            // Ambiente de produção
+        DATABASE_TYPE: 'postgres',
+        DATABASE_URL: 'postgresql://postgres.vspkvsxxytxppdgujdni:@Galmar2552@aws-0-sa-east-1.pooler.supabase.com:6543/postgres',
         MEDUSA_ADMIN_ONBOARDING_TYPE: 'nextjs',
-        ADMIN_CORS: '/https?://.+/i',
-        STORE_CORS: '/https?://.+/i',
-        MEDUSA_BACKEND_URL: 'http://44.203.95.169:9000', // API de produção
+        STORE_CORS: '/http://.+/',
+        ADMIN_CORS: '/http://.+/',
         MEDUSA_ADMIN_ONBOARDING_NEXTJS_DIRECTORY: 'galmar-frontend-store',
+        MEDUSA_BACKEND_URL: 'http://44.203.95.169:9000',
         JWT_SECRET: 'galmar',
         COOKIE_SECRET: 'galmar'
       }
